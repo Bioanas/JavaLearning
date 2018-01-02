3581d02f47953608934f3f23884c94887402fbb4
Bioanas <33870208+Bioanas@users.noreply.github.com>
2018-01-03 00:03:18 +0200

    CJPR_02

---
+++ src/com/ia/test/Debugging.java
@@ -0,0 +1,10 @@
+package com.ia.test;
+
+public class Debugging {
+        public static void main(String[] args)
+        {
+            System.out.println("hello 1");
+            System.out.println("hello 2");
+        }
+    }
+
---
+++ src/com/ia/test/TestClass.java
@@ -0,0 +1,8 @@
+package com.ia.test;
+
+public class TestClass {
+    public static void main(String[] args)
+    {
+        System.out.println ("Hello");
+    }
+}

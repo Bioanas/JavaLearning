aeef1a3468ae5ebd6a812abfc025709f4f18f8b6
Bioanas <33870208+Bioanas@users.noreply.github.com>
2018-01-02 23:15:50 +0200

    Initial commit

---
+++ src/com/ia/Add.java
@@ -0,0 +1,9 @@
+package com.ia;
+
+public class Add {
+    public static void main(String[] args) {
+        /*metoda println()tipăreşte întregul număr la ieşire şi trece în rând nou*/
+        System.out.println(2 + 3);
+    }
+}
+

16f13960dab1182604a68123b8eddba1cdad2e33
Bioanas <33870208+Bioanas@users.noreply.github.com>
2018-01-03 20:47:19 +0200

    CJPR_04_exercise_2

---
+++ src/com/ia/CJPR_04_exercise_2.java
@@ -0,0 +1,9 @@
+package com.ia;
+
+public class CJPR_04_exercise_2 {
+    public static void main (String [] args) {
+        double var1 = 18.757;
+        int var2 = (int)var1;
+        System.out.println(var2);
+    }
+}

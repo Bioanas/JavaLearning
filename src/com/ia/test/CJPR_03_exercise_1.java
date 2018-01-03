c0f49d46bc448bc796503df19922a8120d042f8d
Bioanas <33870208+Bioanas@users.noreply.github.com>
2018-01-03 16:19:53 +0200

    CJPR_03_exercise_2

---
+++ src/com/ia/test/CJPR_03_exercise_1.java
@@ -0,0 +1,22 @@
+package com.ia.test;
+
+public class CJPR_03_exercise_1 {
+    public static void main(String[] args) {
+        /**
+         * prima variabila
+         */
+        int operand1 = 2;
+        /**
+         * a doua variabila
+         */
+        int operand2 = 3;
+        /**
+         * suma celor doua variabile
+         */
+        int sum = operand1 + operand2;
+        /**
+         * afisarea rezultatului si trecerea la o noua linie
+         */
+        System.out.println(sum);
+    }
+}
---
+++ src/com/ia/test/CJPR_03_exercise_2.java
@@ -0,0 +1,13 @@
+package com.ia.test;
+
+public class CJPR_03_exercise_2 {
+    public static void main(String[] args) {
+        int a = 2, b = 3;
+        //System.out.print("First operand: ");
+        //System.out.println(a);
+        //System.out.print("Second operand: ");
+        //System.out.println(b);
+        //System.out.print("Sum: ");
+        System.out.println(a + b);
+    }
+}

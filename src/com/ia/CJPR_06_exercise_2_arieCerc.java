6c49cde9d0be8cb76dd1ee18ef61da2de1b4dcfa
Bioanas <33870208+Bioanas@users.noreply.github.com>
2018-01-04 16:25:52 +0200

    CJPR_06

---
+++ src/com/ia/CJPR_06_exercise_2_arieCerc.java
@@ -0,0 +1,10 @@
+package com.ia;
+
+public class CJPR_06_exercise_2_arieCerc {
+    static final double PI = 3.14;
+    public static void main (String[] args){
+    double raza = 5;
+    double Aria = PI * raza * raza;
+    System.out.println(Aria);
+    }
+}
